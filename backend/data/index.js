ccab9fa7274a21386927934bc79cfd6423040f84
yyamasakii <yumi_yamasaki@mail.toyota.co.jp>
2023-06-01 17:51:15 +0900

    try1

---
+++ backend/data/index.js
@@ -0,0 +1,4 @@
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+const environment = process.env.DATABASE_URL ? "production" : "development";
+module.exports = knex(knexConfig[environment]);
--- backend/data/knexfile.js
+++ backend/data/knexfile.js
@@ -4,50 +4,46 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || "pokemonsearch",
-      user: process.env.DB_USERNAME || "user",
-      password: process.env.DB_PASSWORD || "user",
-    },
-    migrations: {
-      directory: "./migrations",
-      tableName: "knex_migrations",
-    },
-  },
+	development: {
+		client: "pg",
+		connection: {
+			host: process.env.DB_HOST || "127.0.0.1",
+			port: process.env.DB_PORT || 5432,
+			database: process.env.DB_NAME || "pokemonsearch",
+			user: process.env.DB_USERNAME || "user",
+			password: process.env.DB_PASSWORD || "user",
+		},
+		migrations: {
+			directory: "./migrations",
+			tableName: "knex_migrations",
+		},
+	},
 
-  staging: {
-    client: "pg",
-    connection: DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./migrations",
-      tableName: "knex_migrations",
-    },
-  },
+	staging: {
+		client: "pg",
+		connection: DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: "./migrations",
+			tableName: "knex_migrations",
+		},
+	},
 
-  production: {
-    client: "pg",
-    connection: {
-      database: "pokemonsearch",
-      user: "user",
-      password: "user",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./migrations",
-      tableName: "knex_migrations",
-    },
-  },
+	production: {
+		client: "pg",
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: "./migrations",
+		},
+		seeds: { directory: "./seeds" },
+	},
 };
 
 // require("dotenv").config({

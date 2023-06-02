444eb71fe4ad6f042c8de9b2fce0959f6edc6c69
yyamasakii <yumi_yamasaki@mail.toyota.co.jp>
2023-06-02 18:41:36 +0900

    try25

--- backend/data/knexfile.js
+++ backend/data/knexfile.js
@@ -35,10 +35,10 @@ module.exports = {
 	production: {
 		client: "pg",
 		connection: process.env.DATABASE_URL,
-		// pool: {
-		// 	min: 2,
-		// 	max: 10,
-		// },
+		pool: {
+			min: 2,
+			max: 10,
+		},
 		migrations: {
 			directory: "./migrations",
 			tableName: "pokemon",

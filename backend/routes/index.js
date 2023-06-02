6b535569aba38f80cf4230b6dbbcc95488faf52b
yyamasakii <yumi_yamasaki@mail.toyota.co.jp>
2023-06-02 16:45:42 +0900

    try19

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
 	// res.render('index', { title: 'Express' }); //renderによる描画は不要
 	// res.sendFile("../public/index.html"); //クライアントにindex.htmlを返す
-	res.sendFile("../frontend/build/index.html"); //クライアントにindex.htmlを返す
+	res.sendFile("../public/index.html"); //クライアントにindex.htmlを返す
 });
 
 module.exports = router;
